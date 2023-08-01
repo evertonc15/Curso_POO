b93946d4e98a58d0a58567aa0acc51340a410ed2
evertonc15 <everton.ci@outlook.com>
2023-08-01 12:07:40 -0300

    Criando um banco

---
+++ src/Aula05/Aula05.java
@@ -0,0 +1,10 @@
+package Aula05;
+
+public class Aula05 {
+    public static void main(String[] args) {
+        BancoNu p1 = new BancoNu();
+        p1.setNum_Conta(1111);
+        p1.setDono("Everton");
+        p1.estado_Atual();
+    }
+}
