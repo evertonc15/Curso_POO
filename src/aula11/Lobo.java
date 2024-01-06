38299c57bc8785cbd56821aa5fdd497f0382b455
evertonc15 <everton.ci@outlook.com>
2024-01-06 12:54:52 -0300

    Aula 13 concluida - 84% do curso feito

---
+++ src/aula11/Lobo.java
@@ -0,0 +1,10 @@
+package aula11;
+
+import aula10.Mamifero;
+
+public class Lobo extends Mamifero {
+    @Override
+    public void emitirSom() {
+        System.out.println("Auuuuuuuuuuu!");
+    }
+}
---
+++ src/aula11/Mamifero.java
@@ -0,0 +1,8 @@
+package aula11;
+
+public class Mamifero extends Animal{
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Mamifero");
+    }
+}
